20202b350dbe657f50e65f433613f9d1a75509bf
RobiesRoberto <143709591+RobiesRoberto@users.noreply.github.com>
2023-10-02 10:09:01 -0300

    Add files via upload

---
+++ tcc/login.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Robies</title>
+    <link rel="icon" type="image/png" href="img/LOGO.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+    <header>
+    <div class="LOGO">
+    <img src="img/LOGO.png"></img>
+    </div>
+    </header>
+    <div class="main-login">
+        <div class="left-login">
+            <div class="foto">
+            <img src="img/foto entrar.png"></img>
+        </div>
+        </div>
+
+            <div class="right-login">
+        <div class="card-login">
+      
+            <h1>ENTRAR</h1>
+            <div class="textfield">
+
+               <label for="email">Email</label> 
+
+               <input type="text" required name="email" placeholder="Email">
+               <br>
+               <label for="senha">Senha</label> 
+        <input type="text" required name="senha" placeholder="Senha">
+            </div> 
+              
+           <a  href="inicial.html" class="btn-login" >Entrar</a>
+           <a  href="cadastro.php" class="btn-cadastro" >Cadastrar</a>
+      </div>
+        </div>
+    </div>
+    <footer>
+        <div id="footer_content">
+            <div id="footer_contacts">
+                <div class="fotologo">
+                <img src="img/LOGO.png"></img>
+               <p>Redes sociais</p>
+                <div id="footer_social_media">
+              
+                    <a href="#" class="footer-link" id="instagram">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+                    <a href="#" class="footer-link" id="facebook">
+                        <i class="fa-brands fa-facebook" ></i>
+                    </a>   
+                </div>
+            </div> 
+    </footer>
+</div>
+<div id="footer_copyright">
+    &#169
+    2023 all rights reserved
+</div>
+
+</body>
+</html>
